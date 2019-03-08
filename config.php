6df66c3f64428dfb990ff2360df1f1a8ff7c1bc0
dstelangre <dstelangre@gmail.com>
2019-03-08 14:02:39 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $mysql_hostname = "localhost";
 $mysql_user = "dste2385_dnyanu";
-$mysql_password = "d11s8t8dnyanu";
+$mysql_password = "pass";
 $mysql_database = "dste2385_TEST";
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Opps some thing went wrong");
 mysql_select_db($mysql_database, $bd) or die("Error on database connection");
